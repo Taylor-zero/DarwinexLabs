4eb560028cc0f397f888fbd45407378535d7b608
Ellis <taylor.ellis4@gmail.com>
2021-03-06 17:30:06 +0000

    210306

--- tools/Python/Tick-Data/dwx_tickdata_download.py
+++ tools/Python/Tick-Data/dwx_tickdata_download.py
@@ -1,3 +1,4 @@
+# %%
 # -*- coding: utf-8 -*-
 
 """
@@ -46,9 +47,9 @@ import gzip
 
 class DWX_Tick_Data():
     
-    def __init__(self, dwx_ftp_user='<insert your Darwinex username>', 
-                     dwx_ftp_pass='<insert your Darwinex password>',
-                     dwx_ftp_hostname='<insert Darwinex Tick Data FTP host>', 
+    def __init__(self, dwx_ftp_user='EllisTaylor',
+                     dwx_ftp_pass='qBv57vQ7JYx1K8',
+                     dwx_ftp_hostname='tickdata.darwinex.com',
                      dwx_ftp_port=21):
         
         # Dictionary DB to hold dictionary objects in FX/Hour format
@@ -103,5 +104,58 @@ class DWX_Tick_Data():
             _exstr = "Exception Type {0}. Args:\n{1!r}"
             _msg = _exstr.format(type(ex).__name__, ex.args)
             print(_msg)
-    
+
+
+    def _download_asset_(self, _asset='EURUSD', _date='2017-10-01', _hour='22',
+                   _ftp_loc_format='{}/{}_ASK_{}_{}.log.gz',
+                   _verbose=False):
+
+        _file = _ftp_loc_format.format(_asset, _asset, _date, _hour)
+        _key = '{}-{}-{}'.format(_asset, _date, _hour)
+
+        self._virtual_dl = BytesIO()
+
+        if _verbose is True:
+            print("\n[INFO] Retrieving file \'{}\' from Darwinex Tick Data Server..".format(_file))
+
+        try:
+            self._ftpObj.retrbinary("RETR {}".format(_file), self._virtual_dl.write)
+
+            self._virtual_dl.seek(0)
+            _log = gzip.open(self._virtual_dl)
+
+            # Get bytes into local DB as list of lists
+            self._asset_db[_key] = [line.strip().decode().split(',') for line in _log]
+
+            # Construct DataFrame
+            _temp = self._asset_db[_key]
+            self._asset_db[_key] = pd.DataFrame({'ask': [l[1] for l in _temp],
+                                'size': [l[2] for l in _temp]},
+                                index=[pd.to_datetime(l[0], unit='ms', utc=True) for l in _temp])
+
+            # Sanitize types
+            self._asset_db[_key] = self._asset_db[_key].astype(float)
+
+            if _verbose is True:
+                print('\n[SUCCESS] {} tick data for {} (hour {}) stored in self._asset_db dict object.\n'.format(_asset, _date, _hour))
+
+        # Case: if file not found
+        except Exception as ex:
+            _exstr = "Exception Type {0}. Args:\n{1!r}"
+            _msg = _exstr.format(type(ex).__name__, ex.args)
+            print(_msg)
+
     #########################################################################
+
+
+# %%
+
+td = DWX_Tick_Data(dwx_ftp_user='EllisTaylor',
+                  dwx_ftp_pass='qBv57vQ7JYx1K8',
+                  dwx_ftp_hostname='tickdata.darwinex.com',
+                  dwx_ftp_port=21)
+
+td._download_hour_(_asset='EURUSD', _date='2018-10-01', _hour='00')
+
+
+td._asset_db['EURUSD-2018-10-01-15']
